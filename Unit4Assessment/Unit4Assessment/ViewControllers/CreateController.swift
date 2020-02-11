01963528009bf18087122a66fcb40b483cf4c2be
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-11 16:04:45 -0500

    able to create and persist a flash card

--- Unit4Assessment/Unit4Assessment/ViewControllers/CreateController.swift
+++ Unit4Assessment/Unit4Assessment/ViewControllers/CreateController.swift
@@ -10,8 +10,13 @@ import UIKit
 import DataPersistence
 
 class CreateController: UIViewController {
-
+    
     private let createVC = CreateView()
+    
+    private var titleForCard = ""
+    private var cardFact1 = ""
+    private var cardFact2 = ""
+    private var cardFacts = [String]()
     
     override func loadView() {
         view = createVC
@@ -21,27 +26,77 @@ class CreateController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        createVC.cardTitleTextField.delegate = self
+        createVC.cardFact1TextView.delegate = self
+        createVC.cardFact2TextView.delegate = self
+        
         navigationItem.title = "Create Flash Card"
         let createBarButtonItem = UIBarButtonItem(title: "Create", style: .done, target: self, action: #selector(createFlashCard(_:)))
         self.navigationItem.rightBarButtonItem  = createBarButtonItem
-
+        //        createBarButtonItem.isEnabled = false
         createVC.backgroundColor = .systemIndigo
     }
     
-
+    
     @objc func createFlashCard(_ sender: UIBarButtonItem){
-        print("clicked")
-        sender.isEnabled = false
+       
+        cardFacts.append(cardFact1)
+        cardFacts.append(cardFact2)
         
-        do {
-//            try dataPersistence.createItem(createdCard)
+        if !cardFact1.isEmpty && cardFacts.count == 2 {
+            sender.isEnabled = false
             
-        } catch {
-            print("could not save")
-            showAlert(title: "Sorry", message: "This Flash Card could not be favorited")
+            let createdCard = Card(id: "1", quizTitle: titleForCard, facts: cardFacts)
+            
+            do {
+                try dataPersistence.createItem(createdCard)
+                
+            } catch {
+                showAlert(title: "Sorry", message: "This Flash Card could not be saved")
+            }
+            showAlert(title: "Great", message: "This card has been added!!!")
+        } else {
+            showAlert(title: "Oops", message: "Please enter a title as well as two facts!!")
         }
         
     }
+    
+}
 
+extension CreateController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        titleForCard = textField.text ?? ""
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+extension CreateController: UITextViewDelegate {
+    
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        
+        if textView == createVC.cardFact1TextView {
+            cardFact1 = textView.text
+            textView.text = ""
+            textView.resignFirstResponder()
+            return true
+        } else {
+            cardFact2 = textView.text
+            textView.text = ""
+            textView.resignFirstResponder()
+            return true
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView == createVC.cardFact1TextView {
+            cardFact1 = textView.text
+        } else {
+            cardFact2 = textView.text
+        }
+        
+    }
+    
 }
--- Unit4Assessment/Unit4Assessment/Views/CreateView.swift
+++ Unit4Assessment/Unit4Assessment/Views/CreateView.swift
@@ -12,24 +12,33 @@ class CreateView: UIView {
     
     public lazy var cardTitleTextField: UITextField = {
         let textField =  UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
-        textField.placeholder = "Enter text here"
-        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.placeholder = "enter title for card here"
+        textField.font = UIFont.systemFont(ofSize: 20)
         textField.borderStyle = .roundedRect
         textField.autocorrectionType = .no
         textField.keyboardType = .default
         textField.returnKeyType = .done
-//        textField.clearButtonMode = UITextField.ViewMode.whileEditing
         return textField
     }()
     
-    public lazy var cardFact1TextField: UITextView = {
-        let textV = UITextView()
-        return textV
+    public lazy var cardFact1TextView: UITextView = {
+        let textView = UITextView(frame: CGRect(x: 20, y: 100, width: 300, height: 100))
+        textView.textAlignment = NSTextAlignment.justified
+        textView.autocorrectionType = .no
+        textView.font = UIFont.systemFont(ofSize: 20)
+        textView.keyboardType = .default
+        textView.layer.cornerRadius = 5
+        return textView
     }()
     
-    public lazy var cardFact2TextField: UITextView = {
-        let textField = UITextView()
-        return textField
+    public lazy var cardFact2TextView: UITextView = {
+        let textView = UITextView(frame: CGRect(x: 20, y: 100, width: 300, height: 100))
+        textView.textAlignment = NSTextAlignment.justified
+        textView.autocorrectionType = .no
+        textView.font = UIFont.systemFont(ofSize: 20)
+        textView.keyboardType = .default
+        textView.layer.cornerRadius = 5
+        return textView
     }()
     
     override init(frame: CGRect) {
@@ -44,6 +53,8 @@ class CreateView: UIView {
     
     private func commonInit() {
         setupTitleTextField()
+        setupFact1TV()
+        setupFact2TV()
     }
     
     
@@ -58,4 +69,29 @@ class CreateView: UIView {
             cardTitleTextField.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
+    
+    private func setupFact1TV() {
+        addSubview(cardFact1TextView)
+        cardFact1TextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cardFact1TextView.topAnchor.constraint(equalTo: cardTitleTextField.bottomAnchor, constant: 20),
+            cardFact1TextView.leadingAnchor.constraint(equalTo: cardTitleTextField.leadingAnchor),
+            cardFact1TextView.trailingAnchor.constraint(equalTo: cardTitleTextField.trailingAnchor),
+            cardFact1TextView.heightAnchor.constraint(equalToConstant: 120)
+        ])
+    }
+    
+    private func setupFact2TV() {
+        addSubview(cardFact2TextView)
+        cardFact2TextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cardFact2TextView.topAnchor.constraint(equalTo: cardFact1TextView.bottomAnchor, constant: 20),
+            cardFact2TextView.leadingAnchor.constraint(equalTo: cardTitleTextField.leadingAnchor),
+            cardFact2TextView.trailingAnchor.constraint(equalTo: cardTitleTextField.trailingAnchor),
+            cardFact2TextView.heightAnchor.constraint(equalTo: cardFact1TextView.heightAnchor)
+        ])
+    }
+    
 }
